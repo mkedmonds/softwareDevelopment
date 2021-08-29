12af3e0399c42f85e9d89acc896130615b36436c
madeline edmonds <madness981@live.com>
2021-08-29 10:04:40 -0500

    Version 1.3: Updates

--- README.md
+++ README.md
@@ -3,13 +3,30 @@ Assignment: Create a program in your chosen language to have the user input a us
 Chosen programming language: HTML/CSS/JQUERY
 
 Wednesday - 1pm - 2pm
+
     * Brainstorming how usernames should be inputted into the code, and how the text should be displayed.
+
     *Text files created for students.
 
 Friday - 8am - 10 am
+
     * Coding begins
+
         - HTML file created with:
+
             * Dropdown of student usernames.
+
             * Title and intructions on how to use the dropdown to display student papers.
+
         - README page updated to new changes.
+
         - Code Uploaded to Github.
+
+Sunday - 9:30am -
+    * Updated text files with student goal statements
+
+    * Students I have: chigwira, evanCallaghan, davidbee, maddiee, nguyen, danh1234
+
+    * Problem: Jquery ajax programing not updating the site page
+
+    * Soulution: After looking through stack overflow and console.log errors I found that I needed to run my site on my localhost server, not just from the folder.
--- controls.js
+++ controls.js
@@ -1,4 +1,4 @@
-let studentName = ["chigwira", "craig", "dahn123", "davidbee", "evenCallaghan", "giddy_code", "grant", "jacobH", "maddiee", "morganGrant", "nguyen", "nick"]
+let studentName = ["chigwira", "craig", "danh1234", "davidbee", "evanCallaghan", "giddy_code", "grant", "jacobH", "maddiee", "morganGrant", "nguyen", "nick"]
 
 $(document).ready(function() {
     
@@ -14,9 +14,81 @@ $(document).ready(function() {
         
         let sChoice = $("#studNames").val();
 
-        alert("YOU HAVE CHOSEN " + sChoice);
+        //alert("YOU HAVE CHOSEN " + sChoice);
 
         $("#studInfo").html("");
 
-    })
-})
+        if (sChoice == "maddiee") {
+
+            //console.log("maddiee TEST")
+
+            $.ajax({url: "maddiee.txt", success: function(result){
+
+                console.log("maddiee TEST5")
+
+                $("#studInfo").html(result);
+              }});
+        }
+
+        if (sChoice == "chigwira") {
+
+            //console.log("chigwira TEST")
+
+            $.ajax({url: "chigwira.txt", success: function(result1){
+
+                console.log("chigwira TEST5")
+
+                $("#studInfo").html(result1);
+              }});
+        }
+
+        if (sChoice == "evanCallaghan") {
+
+            //console.log("evanCallaghan TEST")
+
+            $.ajax({url: "evanCallaghan.txt", success: function(result2){
+
+                console.log("evanCallaghan TEST5")
+
+                $("#studInfo").html(result2);
+              }});
+        }
+
+        if (sChoice == "davidbee") {
+
+            //console.log("davidbee TEST")
+
+            $.ajax({url: "davidbee.txt", success: function(result3){
+
+                console.log("davidbee TEST5")
+
+                $("#studInfo").html(result3);
+              }});
+        }
+
+        if (sChoice == "nguyen") {
+
+            //console.log("nguyen TEST")
+
+            $.ajax({url: "nguyen.txt", success: function(result4){
+
+                console.log("nguyen TEST5")
+
+                $("#studInfo").html(result4);
+              }});
+        }
+
+        if (sChoice == "danh1234") {
+
+            //console.log("danh1234 TEST")
+
+            $.ajax({url: "danh1234.txt", success: function(result5){
+
+                console.log("danh1234 TEST5")
+
+                $("#studInfo").html(result5);
+              }});
+        }
+
+    });
+});
